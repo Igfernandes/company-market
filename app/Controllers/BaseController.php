b6c1d3125238aa145bc2c695d267892fc4a09b1a
igfernandes <fernandes7.1@outlook.com>
2025-03-07 09:52:33 -0300

    fix: remove code not necessary

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controllers;
 
-use App\Database\Entities\Users\GroupsEntity;
-use App\Database\Mappers\Users\GroupsMapper;
 use CodeIgniter\Controller;
 use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
@@ -23,21 +21,6 @@ use Psr\Log\LoggerInterface;
  */
 class BaseController extends Controller
 {
-    /**
-     * Defines whether user authentication is required or not
-     *
-     * @var boolean
-     */
-    protected $authentication = false;
-
-
-    /**
-     * Object of main configs
-     *
-     * @var array
-     */
-    protected $configs = [];
-
     /**
      * Instance of the main Request object.
      *
@@ -62,30 +45,6 @@ class BaseController extends Controller
         // Do Not Edit This Line
         parent::initController($request, $response, $logger);
 
-        $session = session();
-        $user = $session->get('author');
-
-        if ($this->authentication === true) {
-            if (empty($user))
-                return redirect()->to(ROUTE_LOGOUT);
-
-            $groupsMapper = new GroupsMapper();
-
-            $this->configs = [
-                "title" => "Sport Plataform",
-                "hasAuthentication" => true,
-                "user" => [
-                    "name" => $user->name,
-                    "photo" => $user->photo
-                ],
-                "tab" => "index",
-                "status" => $user->status,
-                "response" => $session->get('response'),
-                "type" => $session->get('type'),
-                "usersGroups" => array_map(fn (GroupsEntity $groups) => $groups->getTitle(), $groupsMapper->mapper()),
-                "companiesGroups" => [ucfirst(lang('Words.clubs')), ucfirst(lang('Words.federations'))]
-            ];
-        }
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
