99967591a52014fa416cf5800b86f6f1073d47f7
igfernandes <fernandes7.1@outlook.com>
2025-03-14 10:07:15 -0300

    chore: Created some adjustments

--- app/Language/pt-br/Api.php
+++ app/Language/pt-br/Api.php
@@ -21,5 +21,25 @@ return [
             "title" => "Confirme o seu e-mail",
             "emailAlreadyExists" => "O e-mail já está sendo utilizado no sistema. Por favor, utilize outro e-mail"
         ]
+    ],
+    "categories" => [
+        "success" => [
+            "post" => "As categorias foram criadas com sucesso"
+        ],
+        "alerts" => [
+            "has_clients" => "As categorias '{categories}' contém clientes. Desvincule antes de continuar."
+        ]
+    ],
+    "clients" => [
+        "success" => [
+            "post" => "O cliente foi criado com sucesso",
+            "patchCategory" => "Agora a categoria da base de clientes selecionada é {name}",
+            "delete" => "O cliente foi deletado com sucesso"
+        ],
+        "invalid" => [
+            "phone" => "O telefone do cliente encontra-se em uso ou inválido.",
+            "required_clients" => "É necessário informar pelo menos um cliente.",
+            "category" => "A categoria selecionada sofreu alterações ou está inválida. Recarregue e tente novamente."
+        ]
     ]
 ];
