0606a9334e2963e004d5989717cca4b7abc70773
igfernandes <fernandes7.1@outlook.com>
2025-03-23 17:58:33 -0300

    feat: Adding permissions in return data users

--- app/Traits/Users/UsersDataTrait.php
+++ app/Traits/Users/UsersDataTrait.php
@@ -3,11 +3,26 @@
 namespace App\Traits\Users;
 
 use App\Database\Entities\Users\UserEntity;
+use App\Database\Entities\Users\UserGroupsEntity;
 
 trait UsersDataTrait
 {
-    public function builder(UserEntity $userEntity): Object
+    /**
+     * 
+     * @param UserEntity $userEntity
+     * @param array{UserGroupsEntity} $usersGroupsEntity
+     */
+    public function builder(UserEntity $userEntity, array $userGroupsEntities): Object
     {
+        $groupsFiltered = \array_filter(
+            $userGroupsEntities,
+            fn(UserGroupsEntity $userGroupEntity) => $userGroupEntity->getUserId() == $userEntity->getId()
+        );
+        $groups = \array_map(
+            fn(UserGroupsEntity $userGroupsEntity) => $userGroupsEntity->getGroup()->getName(),
+            $groupsFiltered
+        );
+
         return  (object)[
             "id" => $userEntity->getId(),
             "name" => $userEntity->getName(),
@@ -16,6 +31,7 @@ trait UsersDataTrait
             "cpf" => $userEntity->getDecryptCpf(),
             "avatar" => $userEntity->getAvatar(),
             "birthdate" => $userEntity->getBirthdate(),
+            "groups" => join(",", $groups),
             "status" => $userEntity->getStatus(),
             "created_at" => $userEntity->getCreatedAt(),
             "updated_at" => $userEntity->getUpdatedAt()
