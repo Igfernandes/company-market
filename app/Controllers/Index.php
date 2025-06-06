9e5c27ee518bd57dbae504cd261d772d392435cf
igfernandes <fernandes7.1@outlook.com>
2025-06-06 15:36:07 -0300

    chore: custom 404 page

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -7,6 +7,17 @@ class Index extends BaseController
 
     public function index()
     {
-        echo view("/mails/services");
+        $uri = service('uri')->getPath();
+        $isApi = str_starts_with($uri, 'api');
+
+        $response = service('response');
+        $response->setStatusCode(NOT_FOUND);
+        $response->setContentType('application/json');
+
+        return $response->setJSON([
+            'status'  => false,
+            'error'   => NOT_FOUND,
+            'message' => "not_found",
+        ])->getBody();
     }
 }
