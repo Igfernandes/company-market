24d30e8826a913cc9d81289ea31bf9cb4fcde3c4
igfernandes <fernandes7.1@outlook.com>
2025-06-20 16:37:45 -0300

    update

--- app/Database/Migrations/2025-06-17-061200_add_column_service_id_in_custom_forms.php
+++ app/Database/Migrations/2025-06-17-061200_add_column_service_id_in_custom_forms.php
@@ -18,8 +18,8 @@ class AddColumnServiceIdInCustomForms extends Migration
                 'null'     => true, // ou false, dependendo da regra de negócio
             ],
         ]);
-
-        $this->db->query("ALTER TABLE {$this->tb_name} ADD CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES services(id)");
+        $db = \Config\Database::connect();
+        $this->db->query("ALTER TABLE {$db->getPrefix()}{$this->tb_name} ADD CONSTRAINT fk_service_id FOREIGN KEY (service_id) REFERENCES services(id)");
     }
 
     public function down()
