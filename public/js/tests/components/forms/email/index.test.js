499dae7259c1840b58ad54b29e3203425da72854
Ícaro <icaro.davila.santos@gmail.com>
2025-07-28 16:04:01 -0300

    feat: add imcomplete email component tests for label state changes and creation validation

---
+++ public/js/tests/components/forms/email/index.test.js
@@ -0,0 +1,9 @@
+// import { HANDLE_TESTS } from "./handle.test.js";
+// import { INIT_TESTS } from "./init.test.js";
+//* 
+// export const TESTS = {
+//   ...INIT_TESTS,
+//   ...HANDLE_TESTS,
+// };
+//*
+
