bc69a73e9a8139275f9822b4939eac0d65c8d558
Ícaro <icaro.davila.santos@gmail.com>
2025-07-28 20:34:30 -0300

    feat: add Checkbox component and corresponding Mock class for testing

---
+++ app/Components/Forms/Checkbox/Mock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Components\Forms\Checkbox;
+
+use function PHPSTORM_META\type;
+
+class Mock
+{
+    public const PROPS = [
+        "name" => "checkbox",
+        "label" => "Checkbox",
+        "id" => "checkbox",
+        "checked" => false,
+        "required" => "true",
+        "value" => "1",
+        "disabled" => false,
+        "readonly" => false,
+        "className" => "",
+        "checked" => false,
+        "attributes" => [],
+    ];
+}
