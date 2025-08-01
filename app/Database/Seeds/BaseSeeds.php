40d7fcdf38a900e4d2515d4e5c84f5f29cfbb3dd
igfernandes <fernandes7.1@outlook.com>
2025-08-01 18:02:11 -0300

    chore: update rules in seeds

--- app/Database/Seeds/BaseSeeds.php
+++ app/Database/Seeds/BaseSeeds.php
@@ -9,13 +9,13 @@ class BaseSeeds extends Seeder
     public function run()
     {
         $this->call('App\Database\Seeds\Users\UsersSeeder');
-        $this->call('App\Database\Seeds\Users\GroupsSeeder');
-        $this->call('App\Database\Seeds\Users\UsersGroupsSeeder');
-        $this->call('App\Database\Seeds\Users\UsersFieldsSeeder');
-        // $this->call('App\Database\Seeds\Users\SettingsPrivacySeeder');
-        $this->call('App\Database\Seeds\Permissions\PermissionsSeeder');
-        $this->call('App\Database\Seeds\Permissions\GroupsPermissionsSeeder');
-        $this->call('App\Database\Seeds\Fields\FieldsGroupsSeeder');
-        $this->call('App\Database\Seeds\Integrations\IntegrationsSeeder');
+        $this->call('App\Database\Seeds\Users\RolesSeeder');
+        $this->call('App\Database\Seeds\Users\UsersRolesSeeder');
+        // $this->call('App\Database\Seeds\Users\UsersFieldsSeeder');
+        $this->call('App\Database\Seeds\Users\SettingsPrivacySeeder');
+        // $this->call('App\Database\Seeds\Permissions\PermissionsSeeder');
+        // $this->call('App\Database\Seeds\Permissions\GroupsPermissionsSeeder');
+        // $this->call('App\Database\Seeds\Fields\FieldsGroupsSeeder');
+        // $this->call('App\Database\Seeds\Integrations\IntegrationsSeeder');
     }
 }
--- app/Database/Seeds/Users/RolesSeeder.php
+++ app/Database/Seeds/Users/RolesSeeder.php
@@ -2,28 +2,28 @@
 
 namespace App\Database\Seeds\Users;
 
-use App\Database\Entities\Users\GroupEntity;
+use App\Database\Entities\Users\RoleEntity;
 use CodeIgniter\Database\Seeder;
 
-class GroupsSeeder extends Seeder
+class RolesSeeder extends Seeder
 {
     protected array $datas = [
         [
             "id" => 1,
-            "name" => "Admin Master",
-            "description" => "The group with all permissions",
+            "name" => "Administrator",
+            "description" => "The role with all permissions",
             "status" => "ACTIVE"
         ],
         [
             "id" => 2,
             "name" => "employee",
-            "description" => "The group with somes permissions",
+            "description" => "The role with somes permissions",
             "status" => "ACTIVE"
         ],
         [
             "id" => 3,
-            "name" => "client",
-            "description" => "The group to user simples",
+            "name" => "guide",
+            "description" => "The role to user simples",
             "status" => "ACTIVE"
         ],
 
@@ -33,7 +33,7 @@ class GroupsSeeder extends Seeder
     {
         foreach ($this->datas as $data) {
             $prefix = getenv('database.default.DBPrefix');
-            $groupsEntity = new GroupEntity();
+            $groupsEntity = new RoleEntity();
 
             $groupsEntity->setId($data['id']);
             $groupsEntity->setName($data['name']);
@@ -44,7 +44,7 @@ class GroupsSeeder extends Seeder
 
             // Simple Queries
             $this->db->query(
-                "INSERT INTO  " . $prefix . "groups (" . join(", ", array_keys($data)) . ") 
+                "INSERT INTO  " . $prefix . "roles (" . join(", ", array_keys($data)) . ") 
                 VALUES (" . join(", ", array_map(fn($column) =>  ":$column:", array_keys($data))) . ") 
                 ON DUPLICATE KEY UPDATE " . join(", ", array_map(fn($column) =>  "$column = values($column)", array_keys($data))),
                 $data
