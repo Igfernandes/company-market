f6d77dfaf1ddcf8a7963cdc413c8c73099f75031
igfernandes <fernandes7.1@outlook.com>
2025-08-01 17:56:36 -0300

    feat: Create component PasswordToggle

--- app/Views/components/shared/forms/password/passwordToggle.php
+++ app/Views/components/shared/forms/password/passwordToggle.php
@@ -18,28 +18,28 @@ if (isset($name) && isset($storeValue[$name]))
 
 ?>
 
-<div class="password-toggle" data-password='visibility'>
-    <div class="input-group relative">
-
+<div class="password-toggle mb-3" data-component="password-toggle" data-password='visibility'>
+    <div class="input-group relative shadow-sm border-gray-200 border-2 rounded-md">
         <input class="form-control w-100 h-[3rem] text-lg px-2 pt-2 rounded-sm outline-accent focus:outline-solid" type="password" name="password" data-password-target
             name="<?= $name ?>"
-            value="<?= isset($name) ? $name : null ?>"
+            value="<?= isset($value) ? $value : null ?>"
             id="<?= $id ?? $name ?>"
             data-label="<?= $label ?>"
             <?= !empty($placeholder) ? "placeholder='$placeholder'" : null ?>
             <?= !empty($attributeData) ? $attributeData : null ?>
             <?= isset($disabled) ? strval($disabled) : null  ?>
             <?= $required ? "required" : null ?>
-            <?= isset($storeValue[$name]) && $storeValue[$name] == $value && isset($type) ?>
             <?= strval($readonly) ?? null ?>>
-        <label class="absolute left-1 top-20" data-label-toggle data-component="password-toggle:label" for="<?= $id ?? $name ?>">
+        <label class="absolute left-1 top-20 pl-1 text-black-700" data-label-toggle data-component="password-toggle:label" for="<?= $id ?? $name ?>">
             <strong>
                 <?= ucfirst($label) ?>
-                <?= isset($required) && $required == "true" ?  view("/components/shared/forms/tooltip/required") : null ?>
+                <?= isset($required) && $required == "true" ?  Component("/components/shared/forms/tooltip/required") : null ?>
 
             </strong>
         </label>
-        <div data-component="password-toggle:visibility" class="input-group-append absolute w-[1rem] h-[1rem] top-20 right-2 z-20 cursor-pointer fill-accent" data-password-visibility='show'>
+        <div data-component="password-toggle:visibility" class="input-group-append absolute w-[.5rem] h-[.5rem] top-20 right-2 z-20 cursor-pointer fill-accent" data-password-visibility='show'>
         </div>
+    </div>
+    <div class="invalid-message text-xs text-red-500 px-2 mt-1" data-invalid="<?= $name ?>">
     </div>
 </div>
