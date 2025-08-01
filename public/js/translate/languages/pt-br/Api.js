ed88d91b903e0d4728b3ab7d2b281ca80728e97c
igfernandes <fernandes7.1@outlook.com>
2025-08-01 17:42:38 -0300

    feat: Create files with translate to operations at JS

---
+++ public/js/translate/languages/pt-br/Api.js
@@ -0,0 +1,12 @@
+export const API = {
+  auth: {
+    success: {
+      post: "Aguarde! Logo você será redirecionado",
+    },
+    invalid: {
+      credentials: "As credenciais encontram-se inválidas",
+      recaptcha:
+        "A página contém recursos desatualizados. Recarregue e tente novamente.",
+    },
+  },
+};
