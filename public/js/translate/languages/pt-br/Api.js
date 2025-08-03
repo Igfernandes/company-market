be3dec198fdd83530f13c46f0686742d0101a641
igfernandes <fernandes7.1@outlook.com>
2025-08-03 14:24:51 -0300

    chore: Adding translate to Api.invalid.recaptcha message

--- public/js/translate/languages/pt-br/Api.js
+++ public/js/translate/languages/pt-br/Api.js
@@ -1,4 +1,8 @@
 export const API = {
+  invalid: {
+    recaptcha:
+      "A página contém recursos desatualizados. Recarregue e tente novamente.",
+  },
   auth: {
     success: {
       post: "Aguarde! Logo você será redirecionado",
