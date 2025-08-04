47bb43741454e505384457f7c8a878112f89b34a
Ícaro <icaro.davila.santos@gmail.com>
2025-08-04 18:01:47 -0300

    feat: add hasTable prop to Head, Scripts, and create Table components; create Mock and Table classes

---
+++ app/Components/Shared/Layouts/Table/Mock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Components\Shared\Layouts\Table;
+
+class Mock
+{
+    public const PROPS = [
+        "id" => "table",
+        "class" => "",
+        "dataTitles" => ["Column 1", "Column 2", "Column 3"],
+        "dataTable" => [
+            ["Row 1 Data 1", "Row 1 Data 2"],
+            ["Row 2 Data 1", "Row 2 Data 2"],
+        ],
+    ];
+}
