14aaa14facd8df01d563d1aab5dcb0268e62dbd2
igfernandes <fernandes7.1@outlook.com>
2025-08-04 19:57:06 -0300

    feat: Create translate to texts and update others files

---
+++ public/js/translate/languages/pt-br/Screens.js
@@ -0,0 +1,14 @@
+export const SCREENS = {
+  default: {
+    service_error:
+      "Ocorreu um problema em nosso sistema. Tente a operação mais tarde.",
+  },
+  auth: {
+    snackbar_title: "Autenticação",
+  },
+  forgot_password: {
+    snackbar_title: "Recuperação de Senha",
+    sending_form: "Enviando Solicitação",
+    awaiting: "Aguarde enquanto estamos enviando a solicitação",
+  },
+};
