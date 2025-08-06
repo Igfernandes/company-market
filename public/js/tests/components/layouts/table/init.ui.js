6bbaa39bdd1e4b5516acd898b1608496aa2eb078
igfernandes <fernandes7.1@outlook.com>
2025-08-06 09:32:51 -0300

    chore: new tests to component table

--- public/js/tests/components/layouts/table/init.ui.js
+++ public/js/tests/components/layouts/table/init.ui.js
@@ -1,4 +1,5 @@
 import { Log } from "../../../libraries/feedback.js";
+import { hasAttributesInElement } from "../../../libraries/validations/has.js";
 
 export const INIT_TESTS = {
   ShouldCreateTable: () => {
@@ -13,6 +14,37 @@ export const INIT_TESTS = {
     return Log("SUCCESS", {
       component: "table",
       message: "A tabela foi criada com sucesso",
+    });
+  },
+  ShouldCreateTable: () => {
+    const table = document.querySelector("[component='table'] table");
+
+    if (!DataTable.isDataTable(table))
+      return Log("ERROR", {
+        component: "table",
+        message: "A tabela não foi instanciada ",
+      });
+
+    return Log("SUCCESS", {
+      component: "table",
+      message: "A tabela foi criada com sucesso",
+    });
+  },
+  ShouldAllProprietiesInTable: () => {
+    const table = document.querySelector("[component='table']");
+    const elementName = "table";
+
+    if (
+      !hasAttributesInElement(
+        ["[data-heads]", "[data-ajax]", "[data-test='test']"],
+        table
+      )
+    )
+      return;
+
+    Log("SUCCESS", {
+      component: elementName,
+      message: `O ${elementName} foi criado com sucesso`,
     });
   },
 };
