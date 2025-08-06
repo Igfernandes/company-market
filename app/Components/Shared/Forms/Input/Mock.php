90709697ecb1073f6ab845441419b1bf65ad2015
Ícaro <icaro.davila.santos@gmail.com>
2025-08-06 19:25:57 -0300

    feat: Add Mock and GroupValidation components for form handling

---
+++ app/Components/Shared/Forms/Input/Mock.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Components\Shared\Forms\Input;
+
+class Mock {
+
+    public const PROPS = [
+        "name" => "input", 
+        "label" => "Input", 
+        "id" => "input",
+        "placeholder" => "",
+        "class" => "",
+        "required" => "true",
+        "attributes" => [],
+        "disabled" => false,
+        "readonly" => false,
+        "icon" => ''
+    ];
+}
