606127414f197282978c9662895f994d7f564fe1
Ícaro <icaro.davila.santos@gmail.com>
2025-08-09 18:16:22 -0300

    feat: warning modal in sidebar

--- app/Views/laboratory/sidebar.php
+++ app/Views/laboratory/sidebar.php
@@ -58,6 +58,9 @@
                     <li class="cursor-pointer" data-navbar='component' data-component="components/utils/modal">
                         <span><i>Modal</i></span>
                     </li>
+                    <li class="cursor-pointer" data-navbar='component' data-component="components/utils/modal/warning">
+                        <span><i>Modal Warning</i></span>
+                    </li>
                 </ul>
             </li>
         </ul>
