3ea1a91de392dc555f540762ef93172bf2ee7d70
igfernandes <fernandes7.1@outlook.com>
2025-08-12 10:04:08 -0300

    chore: update composer, spark and others files config project

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,33 @@
 <?php
 
+use App\Api\Routes\Routes;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+/*
+ * --------------------------------------------------------------------
+ * Router Api
+ * --------------------------------------------------------------------
+ */
+
+$routes->group("api", ['namespace' => 'App'], function ($routes) {
+    $routesApi = new Routes();
+    $routes = $routesApi->init($routes);
+});
+
+/*
+ * --------------------------------------------------------------------
+ * Route Definitions
+ * --------------------------------------------------------------------
+ */
+
+// We get a performance increase by specifying the default
+// route since we don't have to scan directories.
+$routes->get('/', 'Index::index');
+$routes->get('login', 'Index::login');
+$routes->get('forgot-password', 'Index::forgotPassword');
+$routes->get('alter-password', 'Index::alterPassword');
