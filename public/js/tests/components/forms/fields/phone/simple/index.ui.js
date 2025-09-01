b758d7d6e8bdd866c8d3db60a1500a61352ea808
Ícaro <icaro.davila.santos@gmail.com>
2025-09-01 11:49:28 -0300

    fix: correct test path

--- public/js/tests/components/forms/fields/phone/simple/index.ui.js
+++ public/js/tests/components/forms/fields/phone/simple/index.ui.js
@@ -1,7 +1,5 @@
-// import { HANDLE_TESTS } from "./handle.ui.js";
-// import { INIT_TESTS } from "./init.ui.js";
+import { INIT_TESTS } from "./init.ui.js";
 
 export const TESTS = {
-  // ...INIT_TESTS,
-  // ...HANDLE_TESTS,
+  ...INIT_TESTS,
 };
