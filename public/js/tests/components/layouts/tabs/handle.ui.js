8ab9028197a52d3361263cde1bb42eb0aa7a9b0e
Ícaro <icaro.davila.santos@gmail.com>
2025-09-01 11:50:54 -0300

    feat: add ShouldHandleTabClick test

--- public/js/tests/components/layouts/tabs/handle.ui.js
+++ public/js/tests/components/layouts/tabs/handle.ui.js
@@ -1,4 +1,22 @@
 import { Log } from "/js/tests/runtime/feedback.js";
-import { hasAttributesInElement } from "/js/tests/runtime/validations/has.js";
 
-export const HANDLE_TESTS = {};
+export const HANDLE_TESTS = {
+    ShouldHandleTabClick: () => {
+        const tabs = document.querySelectorAll("[tab-target]");
+
+        if (!tabs.length)
+            return Log("ERROR", {
+                component: "tab",
+                message: "O tab não foi encontrado",
+            });
+
+        for (const tab of tabs) {
+            tab.click();
+        }
+
+        return Log("SUCCESS", {
+            component: "tab",
+            message: "O tab foi clicado com sucesso",
+        });
+    },
+};
