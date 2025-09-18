ddac6e384491175c6de8beb69d3bfc21cdba200c
igfernandes <fernandes7.1@outlook.com>
2025-09-18 15:30:35 -0300

    chore: new routes service to users/roles and users/roles/permissions

--- public/js/settings/api.js
+++ public/js/settings/api.js
@@ -10,6 +10,13 @@ export const API_ROUTES = {
     put: "/api/users",
     delete: "/api/users/{id}",
     permanently: "/api/users/trash/{id}",
+    roles: {
+      get: "/api/users/roles/{id}",
+      post: "/api/users/roles",
+      put: "/api/users/roles/{id}",
+      delete: "/api/users/roles/{id}",
+      getPermissions: "/api/users/roles/{id}/permissions",
+    },
   },
   invites: {
     user: "/api/invites/user",
