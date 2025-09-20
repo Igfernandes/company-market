2acf2df87f86ba2aeb05cd3390a29e492bc20136
igfernandes <fernandes7.1@outlook.com>
2025-09-20 09:30:53 -0300

    chore: New translaters response of API

--- public/js/translate/languages/pt-br/Api.js
+++ public/js/translate/languages/pt-br/Api.js
@@ -57,10 +57,16 @@ export const API = {
   },
   roles: {
     invalid: {
+      not_permit:
+        "A função escolhida pertence ao sistema e não pode ser excluída",
+      not_found: "A função não pode ser encontrada ou está inválida",
       already_exists: "A função já está registrada no sistema",
+      already_exists_name: "O nome da função já está sendo utilizado",
     },
     success: {
       post: "Função criada com sucesso",
+      put: "Função atualizada com sucesso",
+      delete: "Função excluída com sucesso",
     },
     permissions: {
       success: {
